38eede05bd9fd6ef9d2030a6e14d58263041ec3a
c-beltran <carlosbeltran90@gmail.com>
2018-08-03 15:39:43 -0400

    added the add a todo functionality

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,12 +1,19 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	//use .parent() to select the parent element of this span
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	//we use this to stop bubbling up to other parents of this element
 	event.stopPropagation();
+});
+
+$("input:text").keypress(function(event){
+	if(event.which === 13){
+		var todo = $(this).val();
+		$('ul').append("<li><span>X </span>" + todo + "</li>");
+	}
 });
